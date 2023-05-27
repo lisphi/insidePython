c4c541a8dbc3760b53f5e5b338d8e0086716b609
benja <lisphi@gmail.com>
2023-05-27 21:59:34 +0800

    weblog

--- ctrpcorp/request_weblog.py
+++ ctrpcorp/request_weblog.py
@@ -4,41 +4,72 @@ from dotenv import load_dotenv
 import os
 import concurrent.futures
 import weblog_crawler as wc
-
  
+
 def count_worker(cookies, app_id, operation, start_time, end_time):
     crawler = wc.create_count_crawler(cookies, app_id, operation)
     return crawler.crawl(start_time, end_time)
 
 
-def main():
-    load_dotenv()
-    cookies = {
-        "sessionid": os.getenv("ES_OPS_SESSIONID"),
-    }
-    app_id = '100017817'
-    operation = 'endHangEvent'
-    start_time = dt.datetime(2023, 5, 21)
-    end_time = start_time + dt.timedelta(days=7)
-    total_seconds = int((end_time - start_time).total_seconds())
-    step_seconds = 1800
-
-    futures = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+def request_count(cookies, app_id, operation, start_time, end_time, total_seconds, step_seconds):
+    count_futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
         for i in range(0, total_seconds, step_seconds):
             current_start_time = start_time + dt.timedelta(seconds = i)
             current_end_time = current_start_time + dt.timedelta(seconds = step_seconds)
             if (current_end_time > end_time):
                 current_end_time = end_time
-            futures.append(executor.submit(count_worker, cookies, app_id, operation, current_start_time, current_end_time))
+            count_futures.append(executor.submit(count_worker, cookies, app_id, operation, current_start_time, current_end_time))
 
-    results = []
-    for future in concurrent.futures.as_completed(futures):
-        results.extend(future.result())
-    results.sort(key=lambda x: x['timestamp'])
-    df = pd.DataFrame(results)
-    file_name = f"ctrpcorp/{app_id}_{operation}_{dt.datetime.strftime(start_time, '%Y%m%d%H%M')}_{int(dt.datetime.now().timestamp())}.csv"
-    df.to_csv(file_name, mode='a', header=False, index=False)
+    count_results = []
+    for future in concurrent.futures.as_completed(count_futures):
+        count_results.extend(future.result())
+    count_results.sort(key=lambda x: x['timestamp'])
+    df = pd.DataFrame(count_results)
+    count_excel = f"ctrpcorp/output/{operation}_{dt.datetime.strftime(dt.datetime.now(), '%m%d%H%M%S')}_count.csv"
+    df.to_csv(count_excel, mode='a', header=True, index=False)
+    return count_results  
+
+
+def detail_worker(cookies, app_id, operation, minute_timestamps):
+    crawler = wc.create_detail_crawler(cookies, app_id, operation)
+    return crawler.crawl_minutes(minute_timestamps)
+
+
+def request_detail(cookies, app_id, operation, minute_start_list):
+    detail_futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        for i in range(0, len(minute_start_list), 30):
+            detail_futures.append(executor.submit(detail_worker, cookies, app_id, operation, minute_start_list[i:i+30]))
+
+    detail_results = []
+    for future in concurrent.futures.as_completed(detail_futures):
+        detail_results.extend(future.result())
+    detail_results.sort(key=lambda x: x['timestamp'])
+    df = pd.DataFrame(detail_results)
+    detail_excel = f"ctrpcorp/output/{operation}_{dt.datetime.strftime(dt.datetime.now(), '%m%d%H%M%S')}_detail.csv"
+    df.to_csv(detail_excel, mode='a', header=True, index=False)
+
+
+def main():
+    load_dotenv()
+    cookies = {
+        "sessionid": os.getenv("ES_COOKIE_SESSION_ID"),
+    }
+    app_id = '100017817'
+    operation = 'endHangEvent'
+    start_time = dt.datetime(2023, 5, 20)
+    end_time = start_time + dt.timedelta(days=8)
+    total_seconds = int((end_time - start_time).total_seconds())
+    step_seconds = 20 * 60 #二十分钟一个任务
+    count_results = request_count(cookies, app_id, operation, start_time, end_time, total_seconds, step_seconds)
+
+    # 从count结果中提取出所有的分钟开始时间，去重，排序
+    minute_start_list = list(set([(count_result['timestamp'] // 60) * 60 for count_result in count_results]))
+    minute_start_list.sort()
+
+    request_detail(cookies, app_id, operation, minute_start_list) 
+
 
 if __name__ == '__main__':
     main()
--- ctrpcorp/weblog_crawler.py
+++ ctrpcorp/weblog_crawler.py
@@ -9,7 +9,7 @@ FROM log.weblog_all
 WHERE {__query_condition}
 GROUP BY t ORDER BY t FORMAT JSON
 """
-    return __Crawler(cookies, count_query_template, app_id, operation, __count_result_convert, 3 * 60)
+    return __Crawler(cookies, count_query_template, app_id, operation, __count_result_convert, 1 * 60)
 
 
 def create_detail_crawler(cookies, app_id, operation):
@@ -31,19 +31,23 @@ class __Crawler:
         self.result_converter = result_converter
         self.max_retries = 3
         self.step_in_second = step_in_second
+        self.headers = {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36' }
+        self.ch_command = 'count' if 'count(' in query_template else 'detail'
         
     def crawl(self, start_time, end_time):
+        log_title = f'{self.operation}-{self.ch_command}'
         total_step = int((end_time - start_time).total_seconds() / self.step_in_second)
         results=[]
         for i in range(total_step):
             current_start_time = start_time + dt.timedelta(seconds = i * self.step_in_second)
             current_end_time = min(start_time + dt.timedelta(seconds = (i + 1) * self.step_in_second), end_time)
-            now_text = dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')
             current_start_time_text = dt.datetime.strftime(current_start_time, '%Y-%m-%d %H:%M:%S')
-            current_end_time_text = dt.datetime.strftime(current_end_time, '%Y-%m-%d %H:%M:%S')
+            current_end_time_text = dt.datetime.strftime(current_end_time, '%H:%M:%S')
             current_time_range_text = f"{current_start_time_text}~{current_end_time_text}"
             if i == 0:
-                print(f"{now_text} -> {current_time_range_text}")
+                print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {log_title} {current_time_range_text}")
             current_start_timestamp = int((current_start_time - dt.datetime(1970, 1, 1)).total_seconds())
             current_end_timestamp = int((current_end_time - dt.datetime(1970, 1, 1)).total_seconds())
             query = self.query_template.format(start=current_start_timestamp, end=current_end_timestamp, app_id=self.app_id, operation=self.operation)
@@ -51,14 +55,14 @@ class __Crawler:
             for i in range(self.max_retries):
                 try:
                     if i > 0:
-                        print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {current_time_range_text}: retying {i} time(s) ...")
-                    response = requests.post(self.url, data=query, cookies=self.cookies)
+                        print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {log_title} {current_time_range_text}: retying {i} time(s) ...")
+                    response = requests.post(self.url, data=query, headers=self.headers, cookies=self.cookies)
                     if response.status_code == 200:
                         break
                     time.sleep(1)    
                 except Exception as e:
                     if i == self.max_retries - 1:
-                        print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {current_time_range_text}: {e}")
+                        print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {log_title} {current_time_range_text}: {e}")
                         break
                     else:
                         time.sleep(1)
@@ -69,29 +73,71 @@ class __Crawler:
                 for item in response_data['data']:  
                     results.append(self.result_converter(item))
             else:
-                print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {current_time_range_text}: {response}")
+                print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {log_title} {current_time_range_text}: {response}")
             time.sleep(0.005)
 
-        return results;
+        return results
+
+
+    def crawl_minutes(self, minute_timestamps):
+        log_title = f'{self.operation}-{self.ch_command}'
+        results=[]
+        for i in range(len(minute_timestamps)):
+            current_start_timestamp = minute_timestamps[i]
+            current_end_timestamp = current_start_timestamp + 60
+            current_start_time = dt.datetime.fromtimestamp(current_start_timestamp)
+            current_end_time = dt.datetime.fromtimestamp(current_end_timestamp)
+            current_start_time_text = dt.datetime.strftime(current_start_time, '%Y-%m-%d %H:%M:%S')
+            current_end_time_text = dt.datetime.strftime(current_end_time, '%H:%M:%S')
+            current_time_range_text = f"{current_start_time_text}~{current_end_time_text}"
+            print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {log_title} {current_time_range_text}")
+
+            query = self.query_template.format(start=current_start_timestamp, end=current_end_timestamp, app_id=self.app_id, operation=self.operation)
+            for i in range(self.max_retries):
+                try:
+                    if i > 0:
+                        print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {log_title} {current_time_range_text}: retying {i} time(s) ...")
+                    response = requests.post(self.url, data=query, cookies=self.cookies)
+                    if response.status_code == 200:
+                        break
+                    time.sleep(1)    
+                except Exception as e:
+                    if i == self.max_retries - 1:
+                        print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {log_title} {current_time_range_text}: {e}")
+                        break
+                    else:
+                        time.sleep(1)
+                        continue
+            
+            if response.status_code == 200:
+                response_data = json.loads(response.text)
+                for item in response_data['data']:  
+                    results.append(self.result_converter(item))
+            else:
+                print(f"{dt.datetime.strftime(dt.datetime.now(), '%Y-%m-%d %H:%M:%S')} -> {log_title} {current_time_range_text}: {response}")
+            time.sleep(0.005)
+
+        return results
 
 
 __query_condition = """(timestamp >= toDateTime({start}) AND timestamp <= toDateTime({end})) AND (`appid` = '{app_id}') AND (cs_uri_stem like '%{operation}%')"""
 
 
 def __detail_result_convert(item):
+    local_time =  dt.datetime.strptime(item['timestamp'], "%Y-%m-%d %H:%M:%S") + dt.timedelta(hours=8)
     return {
         'cs_uri_stem': item['cs_uri_stem'], 
         'cs_referer': item['cs_referer'], 
         's_rootmessageid': item['s_rootmessageid'],
-        'date_time': item['timestamp'],
-        'timestamp': dt.datetime.strptime(item['timestamp'], "%Y-%m-%d %H:%M:%S").timestamp(),
+        'date_time': dt.datetime.strftime(local_time, "%Y-%m-%d %H:%M:%S"),
+        'timestamp': int(local_time.timestamp()),
     }
 
 
 def __count_result_convert(item):
     return {
-        'timestamp': item['t'], 
-        'value': item['value'],
+        'timestamp': int(item['t']), 
+        'value': int(item['value']),
         'date_time': dt.datetime.strftime(dt.datetime.fromtimestamp(int(item['t'])), "%Y-%m-%d %H:%M:%S"),
     }
 
